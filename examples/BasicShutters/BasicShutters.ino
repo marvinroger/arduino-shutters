a682279705a63fba7bd81b91f2062e4855c9d165
Marvin ROGER <nivramdu94@gmail.com>
2017-03-13 22:37:01 +0100

    :sparkles: :racehorse: Initial non-blocking v2.0.0 code

--- examples/BasicShutters/BasicShutters.ino
+++ examples/BasicShutters/BasicShutters.ino
@@ -1,37 +1,42 @@
 #include <EEPROM.h>
 #include <Shutters.h>
 
-void shuttersUp(void)
-{
+unsigned long courseTime = 30 * 1000;
+
+void shuttersUp() {
   Serial.println("Shutters going up.");
   // TODO: Implement the code for the shutters to go up
 }
 
-void shuttersDown(void)
-{
+void shuttersDown() {
   Serial.println("Shutters going down.");
   // TODO: Implement the code for the shutters to go down
 }
 
-void shuttersHalt(void)
-{
+void shuttersHalt() {
   Serial.println("Shutters halted.");
   // TODO: Implement the code for the shutters to halt
 }
 
-Shutters shutters(25.1, shuttersUp, shuttersDown, shuttersHalt);
-
-void setup(void)
-{
-  Serial.begin(9600);
-  #ifdef ESP8266
-  EEPROM.begin(4); // Only one byte will be used, but 4 is the minimum for ESP8266 begin()
-  #endif
-  shutters.begin(); // Might take some time first time, as it will open the shutters
-  shutters.requestLevel(50); // Go to 50%
+uint8_t shuttersGetState() {
+  return 255;
 }
 
-void loop(void)
-{
+void shuttersSetState(uint8_t state) {
+  Serial.print("Saving state ");
+  Serial.print(state);
+  Serial.println(".");
+}
+
+Shutters shutters(courseTime, shuttersUp, shuttersDown, shuttersHalt, shuttersGetState, shuttersSetState);
+
+void setup() {
+  Serial.begin(9600);
+  shutters.begin();
+
+  shutters.setLevel(50); // Go to 50%
+}
+
+void loop() {
   shutters.loop();
 }
